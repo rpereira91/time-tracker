e879fcde460a15c8293628d4855c165cdb1d9b99
Ralph Pereira <rp09rl@brocku.ca>
2019-07-23 16:24:16 -0400

    edited the readme

--- time_tracker_old.py
+++ time_tracker_old.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import random
 from datetime import datetime
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 import os.path
 from os import path
